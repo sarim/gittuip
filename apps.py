f86331f544dc37991e0351a07657146096a9eaec
Sarim Khan <sarim2005@gmail.com>
2013-08-12 19:54:53 +0600

    add details page

--- apps.py
+++ apps.py
@@ -4,8 +4,22 @@ import os
 @get("/")
 def index():
     response.set_header('Content-Type', 'text/html')
-    #os.environ['REMOTE_ADDR']
-    return str(os.environ)
+    ip = os.environ['REMOTE_ADDR']
+    browser = request.headers.get("User-Agent")
+    if browser.startswith("Mozilla/"):
+        redirect("/details")
+    return "Your IP : %s" % ip
+
+@get("/details")
+def details():
+    ip = os.environ['REMOTE_ADDR']
+    ret = ["Your IP : %s" % ip]
+    for key in request.headers:
+        line = "%s : %s" % (key , request.headers.get(key))
+        ret.append(line)
+    
+    return "<br>".join(ret)
+
 
 application = default_app()
 if __name__=="__main__":
